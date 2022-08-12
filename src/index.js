d2ddf812487d31be35f7e7c84571d6f2a220cbab
Weslley <weslley@pop-os.localdomain>
2022-08-12 18:15:07 -0300

    apostila03: fim da apostila

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 const App = ()=>{
     //jsx explica como será o visual do componente
     //Esta sintaxe estranha de tags não é uma string, nem HTML
     // É chamada JSX e é uma extensão de sintaxe para JavaScript. Recomendamos usar JSX com o React para descrever como a UI deveria parecer. JSX pode lembrar uma linguagem de template, mas que vem com todo o poder do JavaScript.
+    
+    const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8};
+
+    const textoDoRotulo = 'Nome:';
+
+    const obterTextoDoBotao = () => "Enviar"
+    
     return(
         //{} escape da expressão jsx
         <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
-            <label htmlFor="nome" style={{display: 'block', marginBottom: 4}}>Nome:</label>
+            <label className="rotulo" htmlFor="nome" style={{display: 'block', marginBottom: 4}}>{textoDoRotulo}</label>
             <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}}/>
-            <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>Enviar</button>
+            <button style={estilosBotao}>{obterTextoDoBotao()}</button>
         </div>
     );
 
